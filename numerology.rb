006144ee55931559e4e2d2223800eb3fbf80c51f
ziv <ziv921@yahoo.com>
2015-10-21 20:18:23 -0400

    fixed some problems

--- numerology.rb
+++ numerology.rb
@@ -14,34 +14,58 @@ return number
 end
  
 def get_message(birth_path_num)
-case birth_path_num
-when 1
-message = "Your numerology number is #{birth_path_num}.\nOne is the leader. The number one indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun."
-when 2
-message = "Your numerology number is #{birth_path_num}.\nThis is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
-when 3
-message = "Your numerology number is #{birth_path_num}.\nThis is the motivatinal person and ambitious."
-when 4
-message = "Your numerology number is #{birth_path_num}.\nThis is the motivatinal person and ambitious."
-when 5
-message = "Your numerology number is #{birth_path_num}.\nThis is the motivatinal person and ambitious."
-when 6
-message = "Your numerology number is #{birth_path_num}.\nThis is the motivatinal person and ambitious."
-when 7
-message = "Your numerology number is #{birth_path_num}.\nThis is the motivatinal person and ambitious."
-when 8
-message = "Your numerology number is #{birth_path_num}.\nThis is the motivatinal person and ambitious."
-when 9
-message = "Your numerology number is #{birth_path_num}.\nThis is the motivatinal person and ambitious."
-else
-message = "Uh oh! Your birth path number is not 1-9!"
-end
+		case birth_path_num
+		when 1
+		message = "Your numerology number is #{birth_path_num}.\nOne is the leader. The number one indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun."
+		when 2
+		message = "Your numerology number is #{birth_path_num}.\nThis is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
+		when 3
+		message = "Your numerology number is #{birth_path_num}.\nThis is the motivatinal person and ambitious."
+		when 4
+		message = "Your numerology number is #{birth_path_num}.\nThis is the motivatinal person and ambitious."
+		when 5
+		message = "Your numerology number is #{birth_path_num}.\nThis is the motivatinal person and ambitious."
+		when 6
+		message = "Your numerology number is #{birth_path_num}.\nThis is the motivatinal person and ambitious."
+		when 7
+		message = "Your numerology number is #{birth_path_num}.\nThis is the motivatinal person and ambitious."
+		when 8
+		message = "Your numerology number is #{birth_path_num}.\nThis is the motivatinal person and ambitious."
+		when 9
+		message = "Your numerology number is #{birth_path_num}.\nThis is the motivatinal person and ambitious."
+		else
+		message = "Uh oh! Your birth path number is not 1-9!"
+		end
 end
  
-
 get '/:birthdate' do
-birthdate = params[:birthdate]
-birth_path_num = get_birth_path_num(birthdate)
-message = get_message(birth_path_num)
-"#{message}"
+  setup_index_view
+end
+
+get '/message/:birth_path_num' do
+  birth_path_num = params[:birth_path_num].to_i
+  @message = Person.get_message(birth_path_num)
+  erb :index
+end
+
+get '/' do
+  erb :form
+end
+
+post '/' do
+  birthdate = params[:birthdate].gsub("-", "")
+  if Person.valid_birthdate(birthdate)
+    birth_path_num = Person.get_birth_path_num(birthdate)
+    redirect "/message/#{birth_path_num}"
+  else
+    @error = "Oops! You should enter a valid birthdate in the form of mmddyyyy. Try again!"
+    erb :form    
+  end
+end
+
+def setup_index_view
+	birthdate = params[:birthdate]
+	birth_path_num = Person.get_birth_path_num(birthdate)
+	@message = Person.get_message(birth_path_num)
+  erb :index
 end
