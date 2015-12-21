caf414babe48fd91cdde516a18b262cbb2bdc861
Ziv Zamechek <ziv921@yahoo.com>
2015-12-21 14:44:11 -0500

    added application helper

--- app/controllers/people_contorller.rb
+++ app/controllers/people_contorller.rb
@@ -4,6 +4,42 @@ get '/people' do
   erb :"/people/index"
 end
 
+get '/people/new' do
+  @person = Person.new
+  erb :"/people/new"
+end
+
+post '/people' do
+  if params[:birthdate].include?("-")
+    birthdate = params[:birthdate]
+  else
+    birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
+  end
+  
+  person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+  redirect "/people/#{person.id}"
+end
+
+get '/people/:id/edit' do
+  @person = Person.find(params[:id])
+  erb :"/people/edit"
+end
+
+put '/people/:id' do
+  person = Person.find(params[:id])
+  person.first_name = params[:first_name]
+  person.last_name = params[:last_name]
+  person.birthdate = params[:birthdate]
+  person.save
+  redirect "/people/#{person.id}"
+end
+
+delete '/people/:id' do
+  person = Person.find(params[:id])
+  person.delete
+  redirect "/people"
+end
+
 get '/people/:id' do
   @person = Person.find(params[:id])
   birth_path_num = Person.get_birth_path_num(@person.birthdate.strftime("%m%d%Y"))
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,38 @@
+module ApplicationHelper
+  def people_form_action(person)
+    if person.new_record?
+      @action = "/people"
+    else
+      @action = "/people/#{person.id}"      
+    end
+    @action
+  end
+  
+  def people_form_method(person)
+    if person.new_record?
+      @method = "post"
+    else
+      @method = "put"
+    end
+    @method
+  end
+  
+  def people_form_class(person)
+    if person.new_record?
+      @class = "new-person"
+    else
+      @class = "edit-person"
+    end
+    @class
+  end
+  
+  def people_form_id(person)
+    if person.new_record?
+      @form_id = "new-person"
+    else
+      @form_id = "person-#{person.id}"
+    end
+    @form_id
+  end
+    
+end
